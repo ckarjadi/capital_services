fb4b0f55acd04c9ce9ed9d7bd661f567cf94dd48
Cody Karjadi <ckarjadi@bu.edu>
2020-02-15 23:06:05 -0500

    .pylintrc: config file for using pylint. README.md: readme file. app/routes.py: added about_us, consulting, contact_us, courses, index, maturity_assessment routes and html templates. changed base.html.

---
+++ README.md
@@ -0,0 +1,85 @@
+# Capital Services - Flask Project
+
+Flask Project for Capital Services (BU MET CS 634);
+
+## Getting Started
+```
+git clone https://github.com/ckarjadi/capital_services
+cd capital_services/
+flask run
+```
+
+### Prerequisites
+
++Python 3.6.5+
+
+Click==7.0
+Flask==1.1.1
+itsdangerous==1.1.0
+Jinja2==2.11.1
+MarkupSafe==1.1.1
+python-dotenv==0.11.0
+Werkzeug==1.0.0
+
+### Installing
+
+install via requirements.txt
+
+```
+pip install -r requirements.txt
+```
+
+### Run the application
+
+```
+flask run
+```
+
+### Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Built With
+
+* [Flask](https://github.com/pallets/flask) - The web framework used
+
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,82 @@
+"""
+routes.py
+routing logic;
+"""
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
-
 def index():
 	"""
 	index render;
 	"""
-	return 'Hello, world!'
+	title = 'Home'
+	user = {'username': 'Bob'}
+	template_name = 'index.html'
+	# posts = [
+	# 	{
+	# 		'author': {'username': 'John'},
+	# 		'body': 'Some statement!'
+	# 	},
+	# 	{
+	# 		'author': {'username': 'Sarah'},
+	# 		'body': 'Another statement!'
+	# 	}
+	# ]
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
+
+@app.route('/maturity_assessment')
+def maturity_assessment():
+	"""
+	MA render;
+	"""
+	title = 'Maturity Assessment'
+	user = {'username': 'Bob'}
+	template_name = 'maturity_assessment.html'
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
+
+@app.route('/consulting')
+def consulting():
+	"""
+	consulting render;
+	"""
+	title = 'Consulting'
+	user = {'username': 'Bob'}
+	template_name = 'consulting.html'
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
+
+@app.route('/courses')
+def courses():
+	"""
+	courses;
+	"""
+	title = 'Courses'
+	user = {'username': 'Bob'}
+	template_name = 'courses.html'
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
+
+@app.route('/contact_us')
+def contact_us():
+	"""
+	contact;
+	"""
+	title = 'Contact Us'
+	user = {'username': 'Bob'}
+	template_name = 'contact_us.html'
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
+
+@app.route('/about_us')
+def about_us():
+	"""
+	about
+	"""
+	title = 'About Us'
+	user = {'username': 'Bob'}
+	template_name = 'about_us.html'
+	kwargs = {'user': user, 'title': title}
+	return render_template(template_name, **kwargs)
