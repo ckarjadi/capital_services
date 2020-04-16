eb772c2083be02be6f361b73fcef3160c1834ecd
Cody Karjadi <ckarjadi@bu.edu>
2020-04-16 16:53:03 -0400

    implemented course-specific links for all courses on the training page

--- app/course_data_from_json.py
+++ app/course_data_from_json.py
@@ -7,7 +7,6 @@ import re
 import datetime
 from app.reformat_json import load_json_file
 
-
 def get_start_end_date(one_class, length):
 	"""
 	get start and end date from one_class data object
@@ -16,6 +15,16 @@ def get_start_end_date(one_class, length):
 	start_date_datetime = datetime.datetime.strptime(start_date, "%Y-%m-%d")
 	end_date_datetime = start_date_datetime + datetime.timedelta(days=length)
 	return start_date, end_date_datetime.strftime(r"%Y-%m-%d")
+
+def get_reviews(reviews):
+	"""
+	make sure there are 4 reviews. if not, duplicate from the front
+	"""
+	num_reviews = len(reviews)
+	assert num_reviews > 0, reviews
+	if num_reviews == 4:
+		return reviews
+	return reviews + reviews[0] * (4 - num_reviews)
 
 def get_course_data_from_json(json_file, course_name):
 	"""
@@ -29,8 +38,10 @@ def get_course_data_from_json(json_file, course_name):
 	start_date, end_date = get_start_end_date(one_class, length)
 	location = f"{one_class['city']}, {one_class['country']}"
 
+	reviews = get_reviews(details['reviews'])
 	return {'icon_file': data['icon_file'], 'course_title': data['course_title'],
 		'start_date': start_date, 'end_date': end_date, 'location': location,
 		'about_this_course': details['about_this_course'],
 		'what_youll_learn': details['what_youll_learn'],
-		'length': length, 'effort': details['effort'], 'cost': details['cost']}
+		'length': length, 'effort': details['effort'], 'cost': details['cost'],
+		'reviews': reviews}
